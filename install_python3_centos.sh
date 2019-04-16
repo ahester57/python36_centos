8e090307a08215d5fb4a208b160831f01422da77
Austin Hester <ahester57@gmail.com>
2019-04-16 17:18:35 -0400

    show pip version, update -> check-update
    Signed-off-by: Austin Hester <ahester57@gmail.com>

--- install_python3_centos.sh
+++ install_python3_centos.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 sudo yum install https://centos7.iuscommunity.org/ius-release.rpm &&
-sudo yum update &&
+sudo yum check-update &&
 sudo yum install python36u python36u-libs python36u-devel python36u-pip &&
 echo
 echo "Python version:"
 python3.6 -V
+echo "pip version:"
+pip3.6 -V
 
