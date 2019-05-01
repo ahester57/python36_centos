e56285008d725500b2f51657a4066983c1c3a575
Austin Hester <ahester@cisco.com>
2019-05-01 11:50:06 -0400

    allow updating pip and install pipenv
    Signed-off-by: Austin Hester <ahester@cisco.com>

--- install_python3_centos.sh
+++ install_python3_centos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install https://centos7.iuscommunity.org/ius-release.rpm &&
+sudo yum install https://centos7.iuscommunity.org/ius-release.rpm
 sudo yum check-update &&
 sudo yum install python36u python36u-libs python36u-devel python36u-pip &&
 echo
@@ -8,4 +8,32 @@ echo "Python version:"
 python3.6 -V
 echo "pip version:"
 pip3.6 -V
+
+while true; do
+    read -p "Update PIP now? " answer
+    case $answer in
+        y|Y|yes|YES)
+            echo "Updating" &&
+            sudo python3.6 -m pip install --upgrade pip
+            break;;
+        n|N|no|NO)
+            break;;
+        *)
+            echo "(y/n) ";;
+    esac
+done
+
+while true; do
+    read -p "Install PIPENV now? " answer
+    case $answer in
+        y|Y|yes|YES)
+            echo "Installing..." &&
+            sudo -H pip3.6 install pipenv
+            break;;
+        n|N|no|NO)
+            break;;
+        *)
+            echo "(y/n) ";;
+    esac
+done
 
